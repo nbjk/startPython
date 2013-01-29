e1b9b6bd26f99a34503c971ec0ad55ee653482d4
LanQ <lanjunq@gmail.com>
2013-01-29 21:57:51 +0800

    前5节练习

---
+++ learnPythonTheHardWay/ex3.py
@@ -0,0 +1,27 @@
+print "I will now count my chickens:"
+
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 * 3 % 4
+
+print "Now I will count the eggs:"
+
+print 3 + 2 + 1 - 5 +4 % 2 - 1 / 4 + 6
+
+print "Is it ture that 3 + 2 < 5 - 7?"
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's false."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
+
+#course 3 more practises
+print "=========================================="
+print "if your salary is 5000"
+print "your tax is:", 5000 * 0.2
+print "you will get total per month:", 5000 - 5000 * 0.2
