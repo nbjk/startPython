a2fb1903e14aad79885a545b0f8ae26746a2acbc
LanQ <lanjunq@gmail.com>
2013-02-14 15:29:45 +0800

    daily lessons

---
+++ learnPythonTheHardWay/ex39.py
@@ -0,0 +1,49 @@
+states = {
+                'Oregon': 'OR',
+                'Florida': 'FL',
+                'California': 'CA',
+                'New York': 'NY',
+                'Michigan': 'MI'
+}
+
+cities = {
+                'CA': 'San francisco',
+                'MI': 'Detroit',
+                'FL': 'Jacksonville'
+}
+
+cities['NY'] = 'New York'
+cities['OR'] = 'Portland'
+
+print '-' * 10
+print "NY State has: ", cities['NY']
+print "OR State has: ", cities['OR']
+
+print '-' * 10
+print "Michigan's abbreviation is: ", states['Michigan']
+print "Florida's abbreviation is: ", states['Florida']
+
+print '-' * 10
+print "Michigan has: ", cities[states['Michigan']]
+print "Florida has:", cities[states['Florida']] 
+
+print '-' * 10
+for state, abbrev in states.items():
+        print "%s is abbreviated %s" % (state, abbrev)
+
+print '-' * 10
+for abbrev, city in cities.items():
+        print "%s is abbreviated %s" % (city, abbrev)
+
+print '-' * 10
+for state, abbrev in states.items():
+        print "%s State is abbreviated %s and has city %s" % (state, abbrev, cities[abbrev])
+
+print '-' * 10
+state = states.get('Texas', None)
+
+if not state:
+        print "Sorry, no Texas."
+
+city = cities.get('TX', 'Does Not Exist')
+print "The city for the state 'TX' is: %s" % city
